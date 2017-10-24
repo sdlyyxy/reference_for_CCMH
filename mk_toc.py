5f0ba982f064ccf52b438e894137c645a7b7d05e
sdlyyxy <sdlyyxy@icloud.com>
2017-10-24 22:47:42 +0800

    Add articles

--- mk_toc.py
+++ mk_toc.py
@@ -4,4 +4,4 @@ for root, dirs, files in os.walk(_root):
     if root != _root:
         break  
     for name in files:
-        print '* [%s](%s)' % (name[:-3], name) 
+        print ('* [%s](%s)' % (name[:-3], name) )
